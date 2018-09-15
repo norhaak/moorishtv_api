97e91253de1b85273fad079f56129e8ee8d6840d
Mounir BARBISS <=>
2018-09-15 13:54:06 +0000

    added offline parsing of tv programs

--- utils.py
+++ utils.py
@@ -9,12 +9,17 @@ from datetime import datetime
 PROG_URL = 'http://www.alaoula.ma/programmes_inst.php'
 
 
+def getCurrentDate():
+    current_date = datetime.now().date()
+    return current_date
+
 def getCurrentTime():
     time_now = (datetime.now()).strftime('%H:%M')
     return time_now
 
 def parseTVPrograms(soup):
     print("Welcome to MoorishTV!\n")
+
 
     prog_time_list = soup.findAll('div', class_='grille_time_off')
     prog_times = []
@@ -39,16 +44,36 @@ def parseTVPrograms(soup):
         print("{} {}".format(prog_time, prog_title))
 
 
+def fetchData(prog_url):
+    content = None
+    try:
+        resp = requests.get(prog_url)
+        if resp.status_code == 200:
+            content = resp.content 
+    except:
+        content = open('content.bin', 'rb').read()
+    return content
+
+def prepareSoup(content):
+    soup = None
+    if content:
+        soup = BeautifulSoup(content, 'html.parser')
+    return soup
+
 if __name__ == '__main__':
-    """
-    resp = requests.get(PROG_URL)
-    if resp.status_code == 200:
-        soup = BeautifulSoup(resp.content, 'html.parser')
-    """
-    content = open('soup.bin', 'rb').read()
-    soup = BeautifulSoup(content, 'html.parser')
-    parseTVPrograms(soup)
-    """
+    soup = None
+    last_update = None
+
+    if last_update != getCurrentDate():
+        last_update = getCurrentDate()
+        content = fetchData(PROG_URL)
+        open('content.bin', 'wb').write(content)
+    else:
+        content = open('content.bin', 'rb').read()
+
+    soup = prepareSoup(content)
+    
+    if soup:
+        parseTVPrograms(soup)
     else:
         print("Error: Unable to connect to remote server {}".format(PROG_URL))
-    """
